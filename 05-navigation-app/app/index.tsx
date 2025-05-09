af9133eadf952ed3ee3c74786306b186afba0f39
SantiagoEspinoza007 <esantiagoespinoza08@gmail.com>
2025-05-09 10:36:07 -0500

    nativewind

---
+++ 05-navigation-app/app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View className="mt-10">
+      <Text className="text-3xl font-extrabold text-red-500">Hola Mundo</Text>
+    </View>
+  )
+};
+
+export default App;
